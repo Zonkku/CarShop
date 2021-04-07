e6b00ee079bacd07ae8579ff0dab504abcc5c9a1
Sonja <sonja.sauvola@gmail.com>
2021-04-07 15:21:35 +0300

    Eka ajo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
+import { AgGridReact } from 'ag-grid-react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import './App.css';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function App() {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetchCars();
+
+  }, []);
+
+  const fetchCars = () => [
+    fetch ('https://carstockrest.herokuapp.com/cars')
+    .then(response => response.json())
+    .then(data => setCars(data._embedded.cars))
+    .catch(err => console.err(err))
+  ]
+
+  const columns = [
+    { field:'brand', sortable: true, filter: true},
+    { field:'model', sortable: true, filter: true},
+    { field:'color', sortable: true, filter: true},
+    { field:'fuel', sortable: true, filter: true},
+    { field:'year', sortable: true, filter: true, width: 100},
+    { field:'price', sortable: true, filter: true, width: 120}
+
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toolbar position="static">
+        <AppBar>
+          <Typography variant="h6">
+            CarShop
+          </Typography>
+        </AppBar>
+      </Toolbar>
+      <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto' }}>
+        <AgGridReact
+          rowData={cars}
+          columnDefs={columns}
+          pagination={true}
+          paginationPageSize={8}
+
+        />
+      </div>
+        
     </div>
   );
 }
